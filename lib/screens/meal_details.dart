0da0e6f9424db6af023ffde4d90d9a9a55d4ff57
nahid246hasan <nahid246hasan@gmail.com>
2023-10-15 19:48:00 +0600

    Implicit animation and use of Hero animation for multiple screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -16,8 +16,8 @@ class MealDetailsScreen extends ConsumerWidget {
   //basically ConsumerStatefulWidget page e ref globally thake tai mention kora lage nah
   //but ConsumerWidget er khetre build method e mention kora lage
   Widget build(BuildContext context, WidgetRef ref) {
-    final favoriteMeals=ref.watch(favoriteMealsProvider);
-    final isFavorite=favoriteMeals.contains(meal);
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
 
     return Scaffold(
       appBar: AppBar(
@@ -39,28 +39,50 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite?Icons.star:Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin:0.7, end:1.0,).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(
               height: 14,
             ),
             Text(
               "Ingredients",
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(
+                color: Theme
+                    .of(context)
+                    .colorScheme
+                    .primary,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             const SizedBox(
               height: 14,
@@ -68,18 +90,32 @@ class MealDetailsScreen extends ConsumerWidget {
             for (final ingredient in meal.ingredients)
               Text(
                 ingredient,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground),
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(
+                    color: Theme
+                        .of(context)
+                        .colorScheme
+                        .onBackground),
               ),
             const SizedBox(
               height: 24,
             ),
             Text(
               "Steps",
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(
+                color: Theme
+                    .of(context)
+                    .colorScheme
+                    .primary,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             const SizedBox(
               height: 14,
@@ -87,12 +123,19 @@ class MealDetailsScreen extends ConsumerWidget {
             for (final step in meal.steps)
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
                 child: Text(
                   step,
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground),
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(
+                      color: Theme
+                          .of(context)
+                          .colorScheme
+                          .onBackground),
                 ),
               ),
           ],
